f244fd798407fa0006c44f4502952e79b3c5ee81
Philip Montgomery <pmontgom@broadinstitute.org>
2015-09-10 22:57:31 -0400

    implemented cheap make-like logic in python for demo

--- pypliant.py
+++ pypliant.py
@@ -1,6 +1,8 @@
 import json
 import websocket
 import tempfile
+import collections
+import os
 
 class RPCException(Exception):
   pass
@@ -72,13 +74,106 @@ class FS(object):
   def listdir(self, path):
     return [x['Name'] for x in self.client.listdir(path)]
 
-client = Pliant(7788)
-fs = FS(client)
+FINISHED = "finished"
+READY = "ready"
+INCOMPLETE = "incomplete"
+
+class Step:
+  def __repr__(self):
+    return "<inputs=%r, outputs=%r>" % (self.inputs, self.outputs)
+
+  def __init__(self, inputs, outputs, callback):
+    self.inputs = inputs
+    self.outputs = outputs
+    self.callback = callback
+    
+  def get_state(self):
+    #print "get_state"
+    has_all_inputs, inputs_min_ts, inputs_max_ts = check_set(self.inputs)
+    has_all_outputs, outputs_min_ts, outputs_max_ts = check_set(self.outputs)
+    
+    #print locals()
+    
+    if has_all_inputs:
+      if has_all_outputs and inputs_max_ts <= outputs_min_ts:
+        return FINISHED
+      else:
+        return READY
+    else:
+      return INCOMPLETE
+
+  def execute(self):
+    self.callback(self.inputs, self.outputs)
+
+def execute(steps):
+  while True:
+    m = collections.defaultdict(lambda: [])
+    for step in steps:
+      m[step.get_state()].append(step)
+
+    #print m
+    
+    if len(m[READY]) == 0:
+      break
+    
+    for step in m[READY]:
+      #print "before execute"
+      step.execute()
+      #print "after execute"
+      #print "state=%s (%s)" % (step.get_state(), step)
+      import time
+#      time.sleep(1)
+      if step.get_state() != FINISHED:
+        raise Exception("Failed:%s"%step)
+
+  if len(m["INCOMPLETE"]) > 0:
+    raise Exception("Could not execute: %s" % m["INCOMPLETE"])
+
+def check_set(files):
+  #print "check_set(%s)" % files
+  mintimestamp = None
+  maxtimestamp = None
+  has_all = True
+  
+  for fn in files:
+    if not os.path.exists(fn):
+      #print "does not exist: %s" % fn
+      has_all = False
+    else:
+      timestamp = os.stat(fn).st_mtime
+      if mintimestamp == None or mintimestamp > timestamp:
+        mintimestamp = timestamp
+      if maxtimestamp == None or maxtimestamp < timestamp:
+        maxtimestamp = timestamp
+  
+  #print has_all, mintimestamp, maxtimestamp
+  return has_all, mintimestamp, maxtimestamp
+
+def mkfn(txt):
+  def fn(input, output):
+    print txt
+    for n in input:
+      fd = open(n)
+      fd.read()
+      fd.close()
+    for n in output:
+      fd = open(n, "w")
+      fd.write("out")
+      fd.close()
+      print "wrote %s" % n
+      assert os.path.exists(n)
+  return fn
+
+execute([Step([], ["b"], mkfn("callback1")), Step(["b"], ["c"], mkfn("callback2"))])
 
 ####################
-fd = fs.open("foo/sample", "w")
-fd.write("hello")
-fd.close()
-print( fs.listdir("foo"))
-fd = fs.open("foo/sample", "r")
-print("file contents: %r" % fd.read())
+#client = Pliant(7788)
+#fs = FS(client)
+#
+#fd = fs.open("foo/sample", "w")
+#fd.write("hello")
+#fd.close()
+#print( fs.listdir("foo"))
+#fd = fs.open("foo/sample", "r")
+#print("file contents: %r" % fd.read())
+
